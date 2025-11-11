aad7717ea3362559585fb5d1ac80eaebd1b19913
Choatkanaphat Dhitpakdekunanon <beawman@gmail.com>
2025-11-11 08:09:23 +0000

    fix module error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['canvas']
-  },
+  serverExternalPackages: ['canvas'],
   images: {
     domains: ['vercel.blob.store'],
   },
